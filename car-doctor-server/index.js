729980c99394fb17708c9e570739129452fa2050
SajeedAninda <sajaninda@gmail.com>
2023-10-26 21:49:39 +0600

    Created Endpoint to delete inventory data

--- car-doctor-server/index.js
+++ car-doctor-server/index.js
@@ -65,7 +65,15 @@ async function run() {
             console.log(result);
             res.send(result);
         });
-
+        app.delete("/checkout/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await checkOutCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
 
 
 
