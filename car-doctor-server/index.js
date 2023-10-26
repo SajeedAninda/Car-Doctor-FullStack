ef1f5c923fa600b9a5c39c554fdda801b6197615
SajeedAninda <sajaninda@gmail.com>
2023-10-26 15:42:06 +0600

    Created Endpoint to get single data

--- car-doctor-server/index.js
+++ car-doctor-server/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const express = require('express')
 let cors = require("cors");
@@ -34,6 +34,15 @@ async function run() {
 
         app.get("/services", async (req, res) => {
             const result = await serviceCollection.find().toArray();
+            res.send(result);
+        });
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await serviceCollection.findOne(query);
+            console.log(result);
             res.send(result);
         });
 
